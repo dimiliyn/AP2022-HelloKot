7ac3263f94d0e5fe5b2a0776078599bf6509b099
Dim <dmitry.iliyn@outlook.com>
2022-12-10 13:18:30 +0400

    Some lines were added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Hey Kot, wanna be caught by Git and uploaded to GitHub?")
+    print("Let's do that!")
+    print(" Go!")
 }
